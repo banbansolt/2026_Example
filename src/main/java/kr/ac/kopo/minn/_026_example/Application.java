f262c74375ec44ace47e9045709a69d82efd873f
AISW-509-204 <banbansolt@gmail.com>
2026-06-09 11:46:43 +0900

    1

--- src/main/java/kr/ac/kopo/minn/_026_example/Application.java
+++ src/main/java/kr/ac/kopo/minn/_026_example/Application.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.minn._026_example;
 
+import kr.ac.kopo.minn._026_example.configure.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
